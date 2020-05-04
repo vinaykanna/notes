9000ddff4cda36385a0d25bdad60f6d987c776c6
vinaykanna <48582080+vinaykanna@users.noreply.github.com>
2020-05-04 12:02:02 +0530

    initial commit
    initial

---
+++ precache-manifest.e7cc031d86523c0f24bda4a5e51b3f68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5e25bbb10a9defa9e011fe9ea5a65dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af8c6ce3cc537e9b4ef7",
+    "url": "/static/css/main.2c19300e.chunk.css"
+  },
+  {
+    "revision": "a992178b6752e22b0492",
+    "url": "/static/js/2.e02344b6.chunk.js"
+  },
+  {
+    "revision": "7bdab496cd9f260b01125fbfa557a79e",
+    "url": "/static/js/2.e02344b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af8c6ce3cc537e9b4ef7",
+    "url": "/static/js/main.05c6270e.chunk.js"
+  },
+  {
+    "revision": "420e90d25d756f6d8af3",
+    "url": "/static/js/runtime-main.e56cac76.js"
+  }
+]);
